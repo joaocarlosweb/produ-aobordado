bb4d0b77bec642f6bc4eedc293500d1e5f79be9f
joaocarlosweb <joaocarloswebcod@gmail.com>
2025-10-07 19:07:40 -0300

    excuindo a rota home

--- app.py
+++ app.py
@@ -366,20 +366,7 @@ def add_usuario():
         'tipo': tipo
     }})
 
-@app.route('/')
-def home():
-    return jsonify({
-        "status": "online",
-        "mensagem": "API de Produção de Bordado está rodando!",
-        "rotas": [
-            "/api/login",
-            "/api/bordadores",
-            "/api/producao",
-            "/api/usuarios",
-            "/api/estatisticas",
-            "/api/exportar"
-        ]
-    })
+
 
 
 if __name__ == '__main__':
